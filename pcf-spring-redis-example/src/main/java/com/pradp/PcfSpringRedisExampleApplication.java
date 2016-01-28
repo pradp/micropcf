cfe9f4fcc1e89c877c51ea8d4a9795de0a31cdce
Pradeep <pradeep.sekhar.m@gmail.com>
2016-01-28 18:46:01 +0530

    redis cache controller added

--- pcf-spring-redis-example/src/main/java/com/pradp/PcfSpringRedisExampleApplication.java
+++ pcf-spring-redis-example/src/main/java/com/pradp/PcfSpringRedisExampleApplication.java
@@ -1,12 +1,50 @@
 package com.pradp;
 
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Resource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PcfSpringRedisExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PcfSpringRedisExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PcfSpringRedisExampleApplication.class, args);
+    }
+}
+
+@RestController
+class RedisController {
+
+    @Resource(name = "redisTemplate")
+    private RedisTemplate<String, byte[]> template;
+
+    @RequestMapping(path = "/rediscache", method = RequestMethod.POST)
+    public void addToCache(@RequestParam String transactionId, @RequestParam String field,
+            @RequestParam String value) {
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.opsForHash().put(transactionId, field, value);
+        template.expire(transactionId, 30, TimeUnit.MINUTES);
+    }
+    
+    @RequestMapping(path = "/rediscache", method = RequestMethod.GET)
+    public Object getFromCache(@RequestParam String transactionId, @RequestParam String field) {
+        template.expire(transactionId, 30, TimeUnit.MINUTES);
+        return template.opsForHash().get(transactionId, field);
+    }
+    
+    @RequestMapping(path = "/cache/exists", method = RequestMethod.GET)
+    public boolean isKeyExists(@RequestParam String key, @RequestParam String field) {
+        return template.opsForHash().hasKey(key, field);
+    }
+
 }
