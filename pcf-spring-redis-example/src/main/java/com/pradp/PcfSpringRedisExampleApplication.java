099e4dd716ac8a1616e31a393412188101391838
Pradeep <pradeep.sekhar.m@gmail.com>
2016-01-28 16:48:06 +0530

    starting off

---
+++ pcf-spring-redis-example/src/main/java/com/pradp/PcfSpringRedisExampleApplication.java
@@ -0,0 +1,12 @@
+package com.pradp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcfSpringRedisExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcfSpringRedisExampleApplication.class, args);
+	}
+}
